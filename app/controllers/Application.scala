65647d81d9198cec2e983bb61255d367bf4fb073
Sebastian Motraghi <sebastian.m@gmail.com>
2013-05-23 23:44:15 -0700

    A couple basics to start from

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,16 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import models.Entry
+
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(List[Entry]()))
   }
-  
+
+  def push = TODO
+
+  def pop = TODO
+
 }
